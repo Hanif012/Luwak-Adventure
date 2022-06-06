0df483c3003b2863a4ac391cab0e5b54d03db11a
Hanif012 <andque12345012@gmail.com>
2022-06-06 23:35:00 +0700

    added UI and Main Menu

---
+++ src/Levels/UI.gd
@@ -0,0 +1,34 @@
+extends Control
+
+onready var pause_overlay := $ColorRect
+
+var is_paused = false setget set_is_paused
+
+func _ready():
+	$ColorRect/vbox/Resume.grab_focus()
+	
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_cancel"):
+		self.is_paused = !is_paused
+		$ColorRect/vbox/Resume.grab_focus()
+
+func set_is_paused(value):
+	is_paused = value
+	get_tree().paused = is_paused
+	pause_overlay.visible = value
+
+
+func _on_Resume_pressed():
+	self.is_paused = false
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+
+func _on_Settings_pressed():
+	$Popup.popup_centered()
+
+
+func _on_Main_Menu_pressed():
+	get_tree().change_scene("res://src/Menu/Main Menu.tscn")
---
+++ src/Menu/Main Menu.gd
@@ -0,0 +1,23 @@
+extends Node
+
+export(PackedScene) var target_scene
+
+
+
+
+func _on_START_pressed():
+		if !target_scene: # is null
+			print("no scene in this door")
+			return	
+		next_level()
+	
+			
+func next_level():
+	var ERR = get_tree().change_scene_to(target_scene)
+	
+	if ERR != OK:
+		print("something failed in the door scene")
+
+
+func _on_QUIT_pressed():
+	get_tree().quit()
